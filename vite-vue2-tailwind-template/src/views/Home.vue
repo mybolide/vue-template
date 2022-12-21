398e9d825151c88a17129783313d48c8d06ad2b1
bolide <bolide@dabaozongdeMacBook-Pro.local>
2022-12-21 17:57:52 +0800

    vite-vue2-tailwind-template

---
+++ vite-vue2-tailwind-template/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-24">
+    <canvas ref="canvas" />
+  </div>
+</template>
+<script>
+import SmoothSignature from "@/utils/smooth-signature";
+import hp from '@/assets/images/left.jpeg'
+const PROPORTION = 0.37
+
+export default {
+  name: 'Home',
+  mounted () {
+    new SmoothSignature(this.$refs.canvas, {
+      bgImage: hp,
+      width: 600 * PROPORTION,
+      height: 3640 * PROPORTION
+    });
+  },
+  methods: {
+    drawCanvasImage (canvas, image) { }
+  },
+}
+</script>
---
+++ vite-vue2-tailwind-template/tailwind.config.js
@@ -0,0 +1,45 @@
+const spacing = require('./tailwind-config/config/spacing')
+const fontSize = require('./tailwind-config/config/fontSize')
+const fontWeight = require('./tailwind-config/config/fontWeight')
+const lineHeight = require('./tailwind-config/config/lineHeight')
+const maxWidth = require('./tailwind-config/config/maxWidth')
+const colors = require('./tailwind-config/config/colors')
+const borderRadius = require('./tailwind-config/config/borderRadius')
+const borderWidth = require('./tailwind-config/config/borderWidth')
+const backgroundSize = require('./tailwind-config/config/backgroundSize')
+const backgroundPosition = require('./tailwind-config/config/backgroundPosition')
+const backgroundImage = require('./tailwind-config/config/backgroundImage')
+const animation = require('./tailwind-config/config/animation')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize,
+    fontWeight,
+    spacing,
+    lineHeight,
+    borderRadius,
+    borderWidth,
+    backgroundSize,
+    backgroundPosition,
+    extend: {
+      ...animation,
+      fontSize,
+      spacing,
+      maxWidth,
+      lineHeight,
+      colors,
+      backgroundImage
+    },
+  },
+  plugins: [
+    require('tailwindcss-important')(),
+    require('@tailwindcss/line-clamp'),
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+}
